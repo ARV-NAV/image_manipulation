9ec9862e8452f98318e9782e310f942d60c590d2
Gregory Sanchez <sanchez.gregory886@gmail.com>
2020-02-26 15:35:16 -0800

    Add error handling with input dict

--- image_transformation.py
+++ image_transformation.py
@@ -1,15 +1,24 @@
 import cv2
 import numpy
 
+from sys import stderr
+from typing import Optional
+from inspect import currentframe, getframeinfo
 
-def rotate_image(img_path: str, orientation: dict) -> numpy.ndarray:
+
+def rotate_image(img_path: str, orientation: dict) -> Optional[numpy.ndarray]:
+    dst, frameinfo = None, None
     img = cv2.imread(img_path, 0)
     rows, cols = img.shape
 
-    roll = orientation.get("roll")
+    try:
+        frameinfo = getframeinfo(currentframe())
+        roll = orientation["test"]
 
-    rot_matrix = cv2.getRotationMatrix2D((cols/2, rows/2), roll, 1)
-    dst = cv2.warpAffine(img, rot_matrix, (cols, rows))
+        rot_matrix = cv2.getRotationMatrix2D((cols/2, rows/2), roll, 1)
+        dst = cv2.warpAffine(img, rot_matrix, (cols, rows))
+    except KeyError as err:
+        print("{0}:{1}: Key Error: unknown key {2}".format(frameinfo.filename, frameinfo.lineno, err), file=stderr)
 
     # Uncomment this for testing
     # cv2.imshow('img', dst)
