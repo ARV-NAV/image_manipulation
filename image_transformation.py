5ccc56e5c0d4c5d63390a063c2635e10a2b211a8
Gregory Sanchez <sanchez.gregory886@gmail.com>
2020-02-28 17:23:12 -0800

    Add transformation matrix creation for tranforming an image

--- image_transformation.py
+++ image_transformation.py
@@ -6,15 +6,86 @@ to then pass to object detection algorithms
 # ================ Built-in Imports ================
 
 from sys import stderr
+from math import cos, sin
 
 # ================ Third Party Imports ================
 
-from numpy import ndarray
+from numpy import ndarray, matmul
 from cv2 import imread, getRotationMatrix2D, warpAffine
 
 # ================ Authorship ================
 
 __author__ = "Gregory Sanchez"
+
+
+def get_transformation_matrix(orientation: dict) -> ndarray:
+    """Get Transformation Matrix
+
+    Creates the matrix transformation for the image based
+    off the orientation data.
+    This was taken from https://stackoverflow.com/a/37279632
+    """
+    roll = orientation["roll"]
+    pitch = orientation["pitch"]
+    yaw = orientation["yaw"]
+    dx, dy, dz = 0, 0, 1
+
+    f = 1  # focal length expressed in pixel units (TODO: Figure out)
+
+    cx, cy = 0, 0  # principal point that is usually at the image center (TODO: Figure out)
+
+    # Camera Calibration Intrinsics Matrix
+    a2 = [
+        [f, 0, cx, 0],
+        [0, f, cy, 0],
+        [0, 0, 1,  0]
+    ]
+
+    # Inverted Camera Calibration Intrinsics Matrix
+    a1 = [
+        [1/f,   0,   -cx/f],
+        [0,     1/f, -cy/f],
+        [0,     0,   0],
+        [0,     0,   1]
+    ]
+
+    # Rotation matrices around the X, Y, and Z axis
+    r_x = [
+        [1,     0,          0,          0],
+        [0,     cos(roll),  -sin(roll), 0],
+        [0,     sin(roll),  -cos(roll), 0],
+        [0,     0,          0,          1]
+    ]
+
+    r_y = [
+        [cos(pitch),    0, sin(pitch),  0],
+        [0,             1, 0,           0],
+        [-sin(pitch),   0, cos(pitch),  0],
+        [0,             0, 0,           1]
+    ]
+
+    r_z = [
+        [cos(yaw), -sin(yaw), 0, 0],
+        [sin(yaw),  cos(yaw), 0, 0],
+        [0,         0,        1, 0],
+        [0,         0,        0, 1]
+    ]
+
+    # Translation matrix
+    t = [
+        [1, 0, 0, dx],
+        [0, 1, 0, dy],
+        [0, 0, 1, dz],
+        [0, 0, 0, 1]
+    ]
+
+    # Compose rotation matrix with (RX, RY, RZ) (RZ * RY * RX)
+    r = matmul(matmul(r_z, r_y), r_x)
+
+    # Final transformation matrix (A2 * (T * (R * A1))
+    h = matmul(a2, matmul(t, matmul(r, a1)))
+
+    return h
 
 
 def rotate_image(img_path: str, orientation: dict) -> ndarray:
