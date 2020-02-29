484188457e9ffac0644b4afc276fa72e20b92493
Gregory Sanchez <sanchez.gregory886@gmail.com>
2020-02-28 17:46:57 -0800

    WIP: update rotate image to use warpPerspective

--- image_transformation.py
+++ image_transformation.py
@@ -7,18 +7,19 @@ to then pass to object detection algorithms
 
 from sys import stderr
 from math import cos, sin
+from typing import Tuple
 
 # ================ Third Party Imports ================
 
 from numpy import ndarray, matmul
-from cv2 import imread, getRotationMatrix2D, warpAffine
+from cv2 import imread, getRotationMatrix2D, warpAffine, warpPerspective, INTER_LANCZOS4
 
 # ================ Authorship ================
 
 __author__ = "Gregory Sanchez"
 
 
-def get_transformation_matrix(orientation: dict) -> ndarray:
+def get_transformation_matrix(orientation: dict, img_szie: Tuple[float, float]) -> ndarray:
     """Get Transformation Matrix
 
     Creates the matrix transformation for the image based
@@ -32,7 +33,7 @@ def get_transformation_matrix(orientation: dict) -> ndarray:
 
     f = 1  # focal length expressed in pixel units (TODO: Figure out)
 
-    cx, cy = 0, 0  # principal point that is usually at the image center (TODO: Figure out)
+    cx, cy = img_szie  # principal point that is usually at the image center (TODO: Figure out)
 
     # Camera Calibration Intrinsics Matrix
     a2 = [
@@ -79,7 +80,7 @@ def get_transformation_matrix(orientation: dict) -> ndarray:
         [0, 0, 0, 1]
     ]
 
-    # Compose rotation matrix with (RX, RY, RZ) (RZ * RY * RX)
+    # Compose rotation matrix with RX, RY, RZ (RZ * RY * RX)
     r = matmul(matmul(r_z, r_y), r_x)
 
     # Final transformation matrix (A2 * (T * (R * A1))
@@ -99,13 +100,18 @@ def rotate_image(img_path: str, orientation: dict) -> ndarray:
     img = imread(img_path, 0)
     rows, cols = img.shape
 
-    try:
-        roll = orientation["roll"]
-        rot_matrix = getRotationMatrix2D((cols/2, rows/2), roll, 1)
-        dst = warpAffine(img, rot_matrix, (cols, rows))
-    except KeyError as err:
-        print("Key Error: unknown key {0}".format(err), file=stderr)
-        dst = img
+    t = get_transformation_matrix(orientation, (rows/2, cols/2))
+    # import pdb; pdb.set_trace()
+    print(t)
+    dst = warpPerspective(img, t, (rows, cols), flags=INTER_LANCZOS4)
+
+    # try:
+    #     roll = orientation["roll"]
+    #     rot_matrix = getRotationMatrix2D((cols/2, rows/2), roll, 1)
+    #     dst = warpAffine(img, rot_matrix, (cols, rows))
+    # except KeyError as err:
+    #     print("Key Error: unknown key {0}".format(err), file=stderr)
+    #     dst = img
 
     return dst
 
@@ -114,9 +120,9 @@ if __name__ == "__main__":
     from cv2 import imshow, waitKey, destroyAllWindows
 
     test_data = {
-        "pitch": 0,
-        "yaw": 0,
-        "roll": 330
+        "pitch": 0.03037297911942005,
+        "yaw": 0.03037297911942005,
+        "roll": 0.03037297911942005
     }
 
     new_img = rotate_image('./images/img_downscale_30.jpg', test_data)
